d4b388b699e1566f37ed3f9363a76d50bb7823d5
puyangsky <puyangsky@pku.edu.cn>
2017-05-11 00:57:43 +0800

    init

---
+++ BlogGraph/TF-IDF/jieba_test.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import jieba
+import os
+import jieba.analyse
+
+file_path = "./test.txt"
+
+
+def index():
+    if os.path.exists(file_path):
+        content = open(file_path, "r").read()
+        print content
+        tags = jieba.analyse.extract_tags(content, topK=20, withWeight=1)
+        for tag in tags:
+            print("tag: %s\t\t weight: %f" % (tag[0], tag[1]))
+
+if __name__ == '__main__':
+    index()
+    # seg_list = jieba.cut("我来到北京清华大学", cut_all=True)
+    # print("Full Mode: " + "/ ".join(seg_list))  # 全模式
