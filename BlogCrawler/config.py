194c065a36f184e55ee2fb094a2045119bee2183
puyangsky <puyangsky@pku.edu.cn>
2018-01-31 00:00:29 +0800

    refactor crawler code

--- BlogCrawler/config.py
+++ BlogCrawler/config.py
@@ -1,2 +1,2 @@
-
 BlogUrl = "http://www.cnblogs.com/"
+tags = ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6']
--- BlogCrawler/crawler.py
+++ BlogCrawler/crawler.py
@@ -1,87 +1,76 @@
 # -*- coding: utf-8 -*-
+from __future__ import print_function
 import re
 
 import requests
 from BlogCrawler import config
-from bs4 import BeautifulSoup
+from lxml import etree
 
 
 class Crawler:
     def __init__(self, blog_name):
         self.blog_name = blog_name
-        self.urls = set([])
+        self.urls = set()
+        self.hub_urls = set()
+        # self.fetch_hub_url()
+        # self.parse_page_url()
 
     @staticmethod
     def download(url):
-        content = requests.get(url).content
-        return content
+        resp = requests.get(url)
+        if resp.status_code == 200:
+            return resp.content
+        return ""
 
-    def parse_url(self, page_url=None):
+    def fetch_hub_url(self, page_url=None):
+        """
+        解析博客分页链接，将分页的所有链接填充到self.urls中
+        """
         # 首页
         if page_url is None:
             page_url = config.BlogUrl + self.blog_name
-            self.urls.add(page_url)
+            self.hub_urls.add(page_url)
         index_page = self.download(page_url)
-        # print index_page
-        # html = BeautifulSoup(index_page, "lxml")
-        # paginations = html.select('div[id="nav_next_page"] a')
-        # paginations = html.find_all(re.compile("<a href=\"(.*?)\">下一页</a>"))
-        # paginations = html.find_all("下一页")
-        paginations = re.findall(".*<a href=\"(.*?)\">下一页</a>.*", index_page)
-        if len(paginations) >= 1:
-            page_url = paginations[0]
-            self.urls.add(page_url)
+        pagination = re.findall(".*<a href=\"(.*?)\">下一页</a>.*", index_page)
+        if len(pagination) >= 1:
+            page_url = pagination[0]
+            self.hub_urls.add(page_url)
             # dfs
-            return self.parse_url(page_url=page_url)
-        elif len(paginations) == 0:
-            # 到达末页
-            self.urls.add(page_url)
-            return self.urls
+            return self.fetch_hub_url(page_url=page_url)
         else:
-            return []
-
-    def test(self):
-        page_url = "http://www.cnblogs.com/puyangsky/default.html?page=2"
-        index_page = self.download(page_url)
-        paginations = re.findall(".*<a href=\"(.*?)\">下一页</a>.*", index_page)
-        if paginations:
-            print paginations
+            # 到达末页
+            self.hub_urls.add(page_url)
+            return self.hub_urls
 
     def parse_page_url(self):
-        self.parse_url()
-        if len(self.urls) == 0:
+        """
+        解析博客的所有文章链接，返回总数和链接列表
+        :return:
+        """
+        if len(self.hub_urls) == 0:
             return
-        uid = 1
-        page_urls = set([])
-        for single_url in self.urls:
-            page = self.download(single_url)
-            html = BeautifulSoup(page, "lxml")
-            posts = html.select('a[class="postTitle2"]')
+        for single_url in self.hub_urls:
+            content = self.download(single_url)
+            if content == "": continue
+            html = etree.HTML(content)
+            posts = html.xpath("//div[@class='postTitle']/a/@href")
             for post in posts:
-                temp_url = post['href']
-                page_urls.add(str(temp_url))
-        for i in page_urls:
-            # print str(uid) + "\t" + i
-            uid += 1
-        return uid, list(page_urls)
+                self.urls.add(post)
+        print("Total urls size: %d" % len(self.urls))
 
-    def parse_single_page(self):
-        page_count, page_urls = self.parse_page_url()
-        if page_count == 0:
-            return
-        # for page_url in page_urls:
-        page_url = page_urls[0]
-        content = self.download(page_url)
-        html = BeautifulSoup(content, "lxml")
-        # print html.prettify()
-        bodys = html.select('div[id="cnblogs_post_body"]')
-        for body in bodys:
-            print body
-            # TODO 获取p、h1-h6
+    def parse_single_page(self, url):
+        """
+        解析单个文章的内容，只取文章主体部分，作为主题生成的原材料
+        """
+        content = self.download(url)
+        if content == "": return
+        # print(content)
+        pattern = re.compile(r'<div id="cnblogs_post_body".*?>(.*?)</div>', re.M | re.X | re.U)
+        match = pattern.search(content)
+        if match:
+            print(match.group(1))
 
 
 if __name__ == '__main__':
     c = Crawler("puyangsky")
-    # urls = c.parse_url()
-    # print urls
-    c.parse_single_page()
+    c.parse_single_page("http://www.cnblogs.com/puyangsky/p/7545291.html")
